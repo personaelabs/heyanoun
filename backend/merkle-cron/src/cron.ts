b27d03d6dfedc1039597563fbd6e7c812530fa0d
lsankar4033 <lsankar4033@gmail.com>
2022-10-23 19:36:40 +0100

    test query

--- backend/merkle-cron/src/cron.ts
+++ backend/merkle-cron/src/cron.ts
@@ -1,1 +1,21 @@
-console.log('Hello World')
+import gql from "graphql-tag";
+
+import { execute } from "../.graphclient";
+
+const testProposals = gql`
+  query proposals {
+    proposals(orderBy: startBlock, orderDirection: desc) {
+      id
+      proposer {
+        id
+      }
+    }
+  }
+`;
+
+async function main() {
+  const result = await execute(testProposals, {});
+  console.log(result["data"]["proposals"]);
+}
+
+main();
