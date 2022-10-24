3aed1b165a7ceff039b2636d204b42261c39ae61
lsankar4033 <lsankar4033@gmail.com>
2022-10-24 10:51:16 +0100

    delegate+owner queries

--- backend/merkle-cron/src/cron.ts
+++ backend/merkle-cron/src/cron.ts
@@ -1,4 +1,5 @@
 import gql from "graphql-tag";
+import { NumericLiteral } from "typescript";
 
 import { execute } from "../.graphclient";
 
@@ -17,11 +18,51 @@ const testProposals = gql`
   }
 `;
 
-async function main() {
+async function test() {
   const result = await execute(testProposals, {});
   console.log(result["data"]["proposals"]);
 
   console.log(`num proposals: ${result["data"]["proposals"].length}`);
 }
 
-main();
+// TODO: future proof against >1000 results per query
+
+function buildDelegatesQuery(block: number, minDelegates: number) {
+  return gql`query {
+    delegates(first: 1000 block: { number: ${block} }, where: {delegatedVotes_gte: ${minDelegates}}) {
+      id
+      delegatedVotes
+    }
+  }`;
+}
+
+function buildOwnersQuery(block: number, minOwned: number) {
+  return gql`query {
+    accounts(first: 1000 block: { number: ${block} }, where: {tokenBalance_gte: ${minOwned}}) {
+      id
+      tokenBalance
+    }
+  }`;
+}
+
+// TODO: error handling for queries?
+async function getAnonymitySet(
+  block: number,
+  minDelegates: number,
+  minOwned: number
+) {
+  const delegatesRes = await execute(
+    buildDelegatesQuery(block, minDelegates),
+    {}
+  );
+  const delegates = delegatesRes["data"]["delegates"];
+  console.log(delegates);
+
+  const ownersRes = await execute(buildOwnersQuery(block, minOwned), {});
+  const owners = ownersRes["data"]["accounts"];
+  console.log(owners);
+}
+
+getAnonymitySet(15811106, 2, 2);
+
+// TODO: merkleize
