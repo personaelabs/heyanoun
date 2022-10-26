3711e40110f80dc9da78dcba73180fea927d0992
lsankar4033 <lsankar4033@gmail.com>
2022-10-26 12:27:39 +0100

    create group types + get latest prop correctly

--- backend/merkle-cron/src/cron.ts
+++ backend/merkle-cron/src/cron.ts
@@ -11,14 +11,52 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// TODO: retrieved from db
-let latestProp = 0;
+// NOTE: assumes that the latest prop in Prop has all relevant groups
+async function getLastFinalizedProp() {
+  const latestProps = await prisma.prop.findMany({
+    orderBy: {
+      num: "desc",
+    },
+    take: 1,
+  });
+
+  if (latestProps.length === 0) {
+    return 0;
+  } else {
+    return latestProps[0].num;
+  }
+}
+
+// NOTE: when we add more group types, will have to think more about where to put this
+async function createGroupTypes() {
+  await prisma.groupType.upsert({
+    where: {
+      name: "1-noun",
+    },
+    update: {},
+    create: {
+      name: "1-noun",
+    },
+  });
+  await prisma.groupType.upsert({
+    where: {
+      name: "2-noun",
+    },
+    update: {},
+    create: {
+      name: "2-noun",
+    },
+  });
+}
 
 // NOTE: idempotent
 async function run() {
-  // TODO: get latest prop from db
+  await createGroupTypes();
 
-  let props = (await executeQuery(buildPropsQuery(latestProp)))["proposals"];
+  const lastProp = await getLastFinalizedProp();
+  console.log(`Last finalized prop: ${lastProp}`);
+
+  let props = (await executeQuery(buildPropsQuery(lastProp)))["proposals"];
   console.log(`Retrieved ${props.length} proposals`);
 
   for (const { id, createdBlock } of props) {
@@ -27,22 +65,18 @@ async function run() {
       "delegates"
     ];
     console.log(`[prop ${id}] - Retrieved ${delegates.length} delegates`);
-
     let owners = (await executeQuery(buildOwnersQuery(createdBlock, 1)))[
       "accounts"
     ];
     console.log(`[prop ${id}] - Retrieved ${owners.length} owners`);
-
     let anonSet1 = Array.from(
       new Set([...delegates.map((d) => d.id), ...owners.map((d) => d.id)])
     );
     // NOTE: both merkle trees use default depth for now
     let tree1 = await buildTreePoseidon(anonSet1);
-
     console.log(
       `[prop ${id}] - anonSet1 size: ${anonSet1.length}, root: ${tree1.root}`
     );
-
     let anonSet2 = Array.from(
       new Set([
         ...delegates.filter((d) => d.delegatedVotes >= 2).map((d) => d.id),
@@ -53,8 +87,7 @@ async function run() {
     console.log(
       `[prop ${id}] - anonSet2 size: ${anonSet2.length}, root: ${tree2.root}`
     );
-
-    // TODO: act on DB!
+    // TODO: create prop, groups
   }
 }
 
