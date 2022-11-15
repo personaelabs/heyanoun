a86ccd998cc3232c633c2a1cccff39ec6955f323
lsankar4033 <lsankar4033@gmail.com>
2022-11-15 14:53:31 -0800

    dummy code for posting to twitter (NOT COMPLETE!)

--- frontend/utils/twitter.ts
+++ frontend/utils/twitter.ts
@@ -2,6 +2,63 @@ import got from "got";
 import crypto from "crypto";
 import OAuth from "oauth-1.0a";
 
-function getAuthHeader() {}
+const manageTweetsURL = "https://api.twitter.com/2/tweets";
 
-async function postTweet() {}
+type TwitResponse = {
+  statusCode: number;
+  body: {
+    data: {
+      id: string;
+    };
+  };
+};
+
+export function getAuthHeader() {
+  const oauth = new OAuth({
+    consumer: {
+      key: process.env.TWITTER_CONSUMER_KEY!,
+      secret: process.env.TWITTER_CONSUMER_SECRET!,
+    },
+    signature_method: "HMAC-SHA1",
+    hash_function: (baseString, key) =>
+      crypto.createHmac("sha1", key).update(baseString).digest("base64"),
+  });
+
+  return oauth.toHeader(
+    oauth.authorize(
+      {
+        url: manageTweetsURL,
+        method: "POST",
+      },
+      {
+        key: process.env.TWITTER_ACCESS_TOKEN_KEY!,
+        secret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+      }
+    )
+  );
+}
+
+// TODO: args based on how we want to format the tweet
+export async function postTweet(message: string) {
+  const authHeader = getAuthHeader();
+
+  const json: any = { text: message };
+  const resp = await got.post(manageTweetsURL, {
+    json,
+    responseType: "json",
+    headers: {
+      Authorization: authHeader["Authorization"],
+      "content-type": "application/json",
+      accept: "application/json",
+    },
+  });
+
+  if (resp.statusCode !== 201) {
+    throw new Error("error posting tweet");
+  } else {
+    // const tweetID = resp.body.data.id;
+    // const tweetURL = `https://twitter.com/${credential.twitter_account}/status/${tweetID}`;
+    // return tweetURL;
+    return "posted tweet!";
+  }
+}
