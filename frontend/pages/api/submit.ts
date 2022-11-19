33587f183bab6caae77215c2f79c78b03f2b8fe7
lsankar4033 <lsankar4033@gmail.com>
2022-11-18 20:05:09 -0800

    WIP proof verification

--- frontend/pages/api/submit.ts
+++ frontend/pages/api/submit.ts
@@ -10,6 +10,9 @@ import {
 } from "../../utils/wasmPrecompute";
 
 import { prisma } from "../../utils/prisma";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+
+import vkey from "../../utils/verification_key.json";
 
 const elliptic = require("elliptic");
 const snarkjs = require("snarkjs");
@@ -19,10 +22,10 @@ const ec = new elliptic.ec("secp256k1");
 export async function verifyProof(
   publicSignals: string[],
   proof: any,
-  vkey: string
+  vkeyJson: any
 ) {
   const proofVerified = await snarkjs.groth16.verify(
-    JSON.parse(vkey),
+    vkey,
     publicSignals,
     proof
   );
@@ -30,25 +33,19 @@ export async function verifyProof(
   return proofVerified;
 }
 
-// TODO: unit test?
-async function verifySignature(
-  publicSignals: string[],
-  publciSigData: PublicSignatureData
+async function verifyRoot(
+  root: string,
+  propId: string,
+  groupType: string
 ): Promise<boolean> {
-  const { TPreComputes, U } = await getSigPublicSignals(publciSigData);
+  const group = await prisma.group.findFirst({
+    where: {
+      propId: Number(propId),
+      typeId: Number(groupType),
+    },
+  });
 
-  // TODO: compare epxected with TPreComputes in publicSignals (need to deserialize from strings)
-  // TODO: compare expectedU with U in publicSignals (need to deserialize from strings)
-
-  return false;
-}
-
-function verifyGroupMatchesRoot(
-  publicSignals: string[],
-  groupId: number
-): boolean {
-  // TODO: get root from publicSignals, match with group in db
-  return false;
+  return group !== null && group.root === root;
 }
 
 export default async function submit(
@@ -61,45 +58,37 @@ export default async function submit(
   }
   console.log(`Received request: ${JSON.stringify(body)}`);
 
+  const root = body.root;
   const proof = body.proof;
-  const publicSignals: string[] = body.publicSignals;
   const publicSignatureData: PublicSignatureData = body.publicSignatureData;
 
-  // console.log("public signals:");
-  // console.log(publicSignals);
+  console.log("Proof: ", proof);
+  console.log("Public signature data ", publicSignatureData);
+  console.log("Root: ", root); // int
 
-  // // TODO: vkey should be static/local
-  // const vkey = "";
+  if (
+    !(await verifyRoot(
+      root,
+      publicSignatureData.eip712Value.propId,
+      publicSignatureData.eip712Value.groupType
+    ))
+  ) {
+    res.status(400).send("merkle root does not match group specified!");
+    return;
+  }
 
-  // NOTE: this should be propId + groupType in the future
-  // const groupId: number = body.groupId; // NOTE: may also be stored w/publicSignals in future
-  // const group = await prisma.group.findUnique({
-  //   where: { id: groupId },
-  // });
-  // if (!group) {
-  //   res.status(404).send("group not found!");
-  //   return;
-  // }
+  const { TPreComputes, U } = await getSigPublicSignals(publicSignatureData);
 
-  // if (!verifyGroupMatchesRoot(publicSignals, groupId)) {
-  //   res.status(400).send("merkle root does not match group specified!");
-  //   return;
-  // }
+  // TODO: make public signals?
+  const publicSignals = {};
 
-  // TODO: put this back in
-  // if (!verifySignature(publicSignals, publicSignatureData)) {
-  //   res.status(400).send("signature artifact verification failed!");
-  //   return;
-  // }
-
-  // if (!verifyProof(publicSignals, proof, vkey)) {
-  //   res.status(400).send("proof verification failed!");
-  //   return;
-  // }
-
-  // TODO: handle posting
-  // 1. post in db
-  // 2. twitter (and other consumers) pass along
+  // steps:
+  // - verify root matches groupType + propId
+  // - compute TPreComputes and U (sig)
+  // - get vkey
+  // - verify proof + public signals (also effectively verifies sig!)
+  // - add to db
+  // - post to twitter
 
   // TODO: potentially link tweet URL, ipfs hash, etc. in json
   res.status(200).json({ success: true });
