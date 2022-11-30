73c8b04147d72b7a1b1ad702ce99fa3d596a889a
Salvino D'Armati <salvino@nounsvision.com>
2022-11-30 15:22:02 -0500

    feat: modify puppeteer packages to work with vercel env

--- frontend/pages/api/createScreenshot.ts
+++ frontend/pages/api/createScreenshot.ts
@@ -1,40 +1,73 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
 
-import { ErrorResponse, PropsPayload } from "../../types/api";
+import { ErrorResponse } from "../../types/api";
 import { clientFactory } from "../../utils/twitter";
+import edgeChromium from "chrome-aws-lambda";
 
-const puppeteer = require("puppeteer"); // Require Puppeteer module
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from "puppeteer-core";
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 const constructURL = (comment: string) => {
+  console.log("constructing URL:");
   const encodedComment = encodeURI(comment);
-  return `http://localhost:3000/screenshot?text=${encodedComment}`;
+
+  console.log({
+    VERCEL_ENV: process.env.VERCEL_ENV,
+    VERCEL_URL: process.env.VERCEL_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    return `http://localhost:3000/screenshot?text=${encodedComment}`;
+  } else {
+    return `https://${process.env.VERCEL_URL}/screenshot?text=${encodedComment}`;
+  }
 };
 
 const Screenshot = async (userComment: string) => {
-  const browser = await puppeteer.launch(); // Launch a "browser"
+  const executablePath =
+    (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
+
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: true,
+  }); // Launch a "browser"
+
   const page = await browser.newPage(); // Open a new page
   await page.goto(constructURL(userComment)); // Go to the website
 
   // There's currently a reace condition in the code that causes the page to
   // be screenshotted before it's fully loaded. This is a workaround.
-  setTimeout(async () => {
-    await page.screenshot({
-      path: "./tmp/twit.jpeg", // Save the screenshot in current directory
-      type: "jpeg",
-      quality: 100, // Set the quality of the screenshot
-      width: 600,
-      height: 600,
-    });
+  return new Promise((resolve) => {
+    setTimeout(async () => {
+      await page.screenshot({
+        path: "./tmp/twit.jpeg", // Save the screenshot in current directory
+        type: "jpeg",
+        quality: 100, // Set the quality of the screenshot
+      });
 
-    await page.close(); // Close the website
+      await page.close(); // Close the website
 
-    await browser.close(); // Close the browser
-  }, 1000);
+      await browser.close(); // Close the browser
+
+      resolve(null);
+    }, 1000);
+  });
 };
 
 const text = `Presumably whatever code changes are required for private voting
 to be possible on the core nouns contracts can instead be
-implemented on an intermediate contract “above”? This is a test screenshot to be able to see how things work!.`;
+implemented on an intermediate contract “above”? This is a test screenshot to be able to see how things work!
+Hi, it's sal. I think?`;
 
 const request = async (
   _req: NextApiRequest,
@@ -43,11 +76,11 @@ const request = async (
   try {
     await Screenshot(text);
 
-    const client = clientFactory();
-    const mediaId = await client.v1.uploadMedia("./tmp/twit.jpeg");
-    await client.v2.tweet("", {
-      media: { media_ids: [mediaId] },
-    });
+    // const client = clientFactory();
+    // const mediaId = await client.v1.uploadMedia("./tmp/twit.jpeg");
+    // await client.v2.tweet("", {
+    //   media: { media_ids: [mediaId] },
+    // });
 
     res
       .status(200)
