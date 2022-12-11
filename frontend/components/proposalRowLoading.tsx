7005262d5c87babd762d0931023a2755686d9135
christianturner <christianturner@hey.com>
2022-12-10 21:51:44 -0500

    fix loading row ui overflow on mobile

--- frontend/components/proposalRowLoading.tsx
+++ frontend/components/proposalRowLoading.tsx
@@ -6,7 +6,7 @@ export const ProposalRowLoading = ({ count = 5 }: { count: number }) => {
         <div className="rounded-md transition-all shadow-sm bg-white p-3 md:p-7 flex items-center justify-between border border-gray-200 animate-pulse">
           <div className="flex justify-center items-center text-gray-800">
             <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-200 w-20 mr-12"></div>
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-200 w-96"></div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-200 w-36 sm:w-96 md:w-[500px]"></div>
           </div>
           <div className="space-x-0 md:space-x-2 space-y-2 md:space-y-0">
             <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-200 w-12"></div>
